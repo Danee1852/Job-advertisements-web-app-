91117bb5f8678ba16be76305fe388aa30f9d4274
Danee1852 <pawello1852@gmail.com>
2022-02-19 18:03:51 +0100

    Project initiated with Spring Initializr

---
+++ src/main/java/com/example/job/advertisements/web/app/JobAdvertisementsWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.job.advertisements.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAdvertisementsWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAdvertisementsWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/job/advertisements/web/app/JobAdvertisementsWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job.advertisements.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAdvertisementsWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
